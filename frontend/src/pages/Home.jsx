cdaed122178a7063de2a0c99a16886d12a72c6c0
Arpita rao <raoarpita316@gmail.com>
2026-07-06 17:58:39 +0530

    Fix Home conflict markers

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,22 +1,12 @@
-
-<<<<<<< HEAD
 import Hero from "../componets/Hero";
-=======
->>>>>>> 7bb25e6 (Fix Appointment model import)
-
 import ServicesSection from "../componets/ServicesSection";
-import DoctorCard from "../componets/Doctorcard";
 import Testimonials from "../componets/Testimonials";
 
 function Home() {
   return (
     <>
       <Hero />
-      
       <ServicesSection />
-
-      {/* Doctors Section */}
-
       <Testimonials />
     </>
   );
