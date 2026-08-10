d7828156683674b83b46bb28e1094199e8de2a12
Arpita rao <raoarpita316@gmail.com>
2026-08-10 18:20:33 +0530

    fix: correct contact model import

--- backend/routes/contactroutes.js
+++ backend/routes/contactroutes.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const router = express.Router();
-
 const Contact = require("../models/contact");
 const authMiddleware = require("../middleware/authMiddleware");
 
